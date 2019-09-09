43202c239945f18fe0ece21cf5d24686698729c0
Boris Vinogradov <no111u3@gmail.com>
2019-09-09 08:57:55 +0300

    Change hardcoded meta to package env

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use clap::{Arg, App, SubCommand};
 
 fn main() {
     let matches = App::new("Sync your e-book library")
-        .version("0.1.0")
-        .author("Boris V. <no111u3@gmail.com>")
-        .about("Synchonize e-book with your local e-library")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .get_matches();
 }
