19cf2615338f9f31baa9e9b8156635fc042183dd
Boris Vinogradov <no111u3@gmail.com>
2019-09-09 19:31:04 +0300

    Adding scan_area method and it's test

--- src/updater.rs
+++ src/updater.rs
@@ -1,3 +1,4 @@
+use crate::bookshelf::Bookshelf;
 use crate::indexer::Indexer;
 
 pub struct Updater {
@@ -13,6 +14,13 @@ impl Updater {
         }
     }
 
+    fn scan_area(&self) -> (Bookshelf, Bookshelf) {
+        (
+            Indexer::new(self.local.clone()).index(),
+            Indexer::new(self.foreign.clone()).index()
+        )
+    }
+
     pub fn update(&self) {
     }
 }
@@ -23,9 +31,27 @@ mod tests {
 
     #[test]
     fn scan_area() {
+        use crate::book::Book;
+    
         let uper = Updater::new(
             "tests/scan_area/local".to_string(),
             "tests/scan_area/foreign".to_string()
         );
+
+        let scan_a = uper.scan_area();
+
+        let ixer_res: Vec<_> = scan_a.0.iter().cloned().collect();
+        assert_eq!(ixer_res, [
+            Book::new(String::from("file_one.txt")),
+            Book::new(String::from("file_three.txt")),
+            Book::new(String::from("file_two.txt")),
+        ]);
+
+        let ixer_res: Vec<_> = scan_a.1.iter().cloned().collect();
+        assert_eq!(ixer_res, [
+            Book::new(String::from("file_four.txt")),
+            Book::new(String::from("file_one.txt")),
+            Book::new(String::from("file_two.txt")),
+        ]);
     }
 }
