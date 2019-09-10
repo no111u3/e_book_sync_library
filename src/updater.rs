39d276189064a0eac3e7b773b764370157a54a98
Boris Vinogradov <no111u3@gmail.com>
2019-09-10 12:26:06 +0300

    Adding copying to Updater and its test

--- src/updater.rs
+++ src/updater.rs
@@ -1,9 +1,18 @@
+use std::fs;
+use std::path::PathBuf;
+
 use crate::bookshelf::Bookshelf;
 use crate::indexer::Indexer;
 
 pub struct Updater {
     local: String,
     foreign: String,
+}
+
+pub enum Update {
+    OnlyFromLocal,
+    OnlyFromForeign,
+    Bidirectional,
 }
 
 impl Updater {
@@ -22,13 +31,38 @@ impl Updater {
         (local.difference(&foreign), foreign.difference(&local))
     }
 
-    pub fn update(&self) {
+    fn copy_files(&self, books: Bookshelf, destination: PathBuf) {
+        for b in books.iter() {
+            let mut dest_path = destination.to_path_buf();
+            dest_path.push(b.get_path().file_name().unwrap().to_str().unwrap());
+            match fs::copy(b.get_path(), dest_path) {
+                Err(e) => panic!("Copy error: {}", e),
+                Ok(_) => (),
+            }
+        }
+    }
+
+    pub fn update(&self, update: Update) {
         let (from_local, from_foreign) = self.cross_diff(self.scan_area());
+        match update {
+            Update::OnlyFromLocal => {
+                self.copy_files(from_local, PathBuf::from(self.foreign.clone()))
+            }
+            Update::OnlyFromForeign => {
+                self.copy_files(from_foreign, PathBuf::from(self.local.clone()))
+            }
+            Update::Bidirectional => {
+                self.copy_files(from_local, PathBuf::from(self.foreign.clone()));
+                self.copy_files(from_foreign, PathBuf::from(self.local.clone()));
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::Updater;
     use crate::book::Book;
 
@@ -76,5 +110,43 @@ mod tests {
 
         let ixer_res: Vec<_> = from_foreign.iter().cloned().collect();
         assert_eq!(ixer_res, [Book::new(String::from("file_four.txt")),]);
+    }
+
+    #[test]
+    fn copy_files() {
+        let uper = Updater::new(
+            "tests/copy_files/local".to_string(),
+            "tests/copy_files/foreign".to_string(),
+        );
+
+        let delete: std::io::Result<()> = (|| {
+            fs::remove_file("tests/copy_files/foreign/file_one.txt")?;
+            fs::remove_file("tests/copy_files/foreign/file_two.txt")?;
+            fs::remove_file("tests/copy_files/foreign/file_three.txt")?;
+            Ok(())
+        })();
+        match delete {
+            Err(e) => panic!("Delete error: {}", e),
+            Ok(_) => (),
+        }
+
+        let (from_local, from_foreign) = uper.cross_diff(uper.scan_area());
+
+        let ixer_res: Vec<_> = from_local.iter().cloned().collect();
+        assert_eq!(
+            ixer_res,
+            [
+                Book::new(String::from("file_one.txt")),
+                Book::new(String::from("file_three.txt")),
+                Book::new(String::from("file_two.txt")),
+            ]
+        );
+
+        uper.copy_files(from_local, from_foreign.get_path().to_path_buf());
+
+        let (from_local, from_foreign) = uper.cross_diff(uper.scan_area());
+
+        let ixer_res: Vec<_> = from_local.iter().cloned().collect();
+        assert_eq!(ixer_res, []);
     }
 }
