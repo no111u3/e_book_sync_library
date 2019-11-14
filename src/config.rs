5adf0a6c46a3387126e01c5dc00bdd7e1de89b1f
Boris Vinogradov <no111u3@gmail.com>
2019-11-14 12:33:22 +0300

    Add store option for Config

--- src/config.rs
+++ src/config.rs
@@ -46,6 +46,24 @@ impl Config {
 
         Ok((source, destination))
     }
+
+    pub fn store(&self, source: PathBuf, destination: PathBuf) -> Result<(), String> {
+        let mut f = File::create(self.path.clone())
+            .or_else(|e| Err(format!("fail to create config with error: {}", e)))?;
+
+        let config = ConfigStorage {
+            source,
+            destination,
+        };
+
+        let serialized = serde_yaml::to_string(&config)
+            .or_else(|e| Err(format!("fail to serialize config with error: {}", e)))?;
+
+        match f.write_all(serialized.as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(format!("fail to store config with error: {}", e)),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -57,6 +75,21 @@ mod tests {
     #[test]
     fn parse() {
         let cfg = Config::new(PathBuf::from("tests/config/test_config.yaml"));
+
+        let (source, destination) = cfg.parse().unwrap();
+
+        assert_eq!(source, PathBuf::from("/test/path/to_source"));
+        assert_eq!(destination, PathBuf::from("/test/path/to_destination"));
+    }
+
+    #[test]
+    fn store() {
+        let cfg = Config::new(PathBuf::from("tests/config/test_config_store.yaml"));
+
+        let source = PathBuf::from("/test/path/to_source");
+        let destination = PathBuf::from("/test/path/to_destination");
+
+        assert_eq!(cfg.store(source.clone(), destination.clone()), Ok(()));
 
         let (source, destination) = cfg.parse().unwrap();
 
